8e2e21d00ad41c09ba1d9a7bd81f64920e5a6896
Jared Russell <jaredhrussell2016@gmail.com>
2022-06-17 13:33:08 -0400

    Change game to play five rounds

--- app.js
+++ app.js
@@ -7,34 +7,51 @@ else if (choice === 1)
 else if (choice === 2)
     return 'scissor';
 }
-
-let playerSelection = prompt("Enter rock, paper, or scissor: ");
-playerSelection = playerSelection.toLocaleLowerCase();
-let flag = false;
-while (!flag){
-    if ((playerSelection === "rock") || (playerSelection === "paper") || (playerSelection === "scissor"))
-        flag = true;
-    else{
-        playerSelection = prompt("Please enter a valid option: rock, paper, or scissor: ");
+function playerInitial(){
+    let playerSelection = prompt("Enter rock, paper, or scissor: ");
+    playerSelection = playerSelection.toLowerCase();
+    let flag = false;
+    while (!flag){
+        if ((playerSelection === "rock") || (playerSelection === "paper") || (playerSelection === "scissor"))
+            flag = true;
+        else{
+            playerSelection = prompt("Please enter a valid option: rock, paper, or scissor: ");
+        }
     }
+    return playerSelection;
 }
 //const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(computerSelection);
-function playRound (playerSelection, computerPlay) {
-    let  player = playerSelection.toLowerCase();
-	let comp = computerSelection;
+//const computerSelection = computerPlay();
+function playRound() {
+    let  player = playerInitial();
+	let comp = computerPlay();
     if ( (player == "rock" && comp == "paper") || (player == "paper" && comp == "scissor") || (player == "scissor" && comp == "rock")){
-        return `You lost :( You picked ${player} and the computer picked ${comp}`;
+        return "comp";
     }
     else if ((player == "rock" && comp == "rock") || (player == "paper" && comp == "paper") || (player == "scissor" && comp == "scissor")){
-        return `It\'s a tie! You picked ${player} and the computer picked ${comp}`
+        return "tie";
     }
     else {
-        return `You won! You picked ${player} and the computer picked ${comp}`;
+        return "player";
     }
  }
 
- console.log(playRound(playerSelection, computerSelection))
+function game() {
+    let playerWins = 0;
+    let compWins = 0;
+    let ties = 0;
+    for (let i = 0; i < 5; i++){
+        let result = playRound();
+        if (result === "comp")
+            compWins++;
+        else if (result === "tie")
+            ties++;
+        else
+            playerWins++;
+    }
+    return `Player won ${playerWins} times, computer won ${compWins} times, and tied ${ties} times.`;
+}
+
+ console.log(game())
 
 
